efeb1d09df3b487d58cd89e6539458ea84950dfd
Blue Lindsey <linzblue5@gmail.com>
2020-03-30 14:39:11 -0500

    Most recent styles

--- src/components/Issue.jsx
+++ src/components/Issue.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
+// import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container'
 // import CardBody from 'react-bootstrap/Card'
 // import CardColumns from 'react-bootstrap/CardColumns'
 import './Issue.css';
@@ -11,9 +12,9 @@ const Issue = (props) => {
     console.log(props)
     return (
         <li className="pcontainer">
-            <Card><h3 className="cardTitle"><a href={props.issue.html_url}>{props.issue.title}</a></h3>
-                <Container-small> <div className="scroll-box"><p>{props.issue.body}</p></div></Container-small>
-            </Card>
+            <h3 className="cardTitle"><a href={props.issue.html_url}>{props.issue.title}</a></h3>
+            <Container> <div className="scroll-box"><p>{props.issue.body}</p></div></Container>
+
         </li>
 
     )
